1e0c3db4c0d1a4e37e4d6c0446188572022d3844
Lozano 7 <lozanoj914@gmail.com>
2021-11-01 12:30:13 -0500

    haciendo que cambie cuando se le da click en la card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
+const getDog = async (url, card, setCard) => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) throw new Error(response.status);
+
+    let [data] = await response.json();
+    const newCard = {
+      ...card,
+      name: data['breeds'][0]['name'],
+      url: data.url,
+    };
+    setCard(newCard);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const Card = ({ dog }) => {
   const [card, setCard] = useState({
     name: '',
@@ -7,30 +25,26 @@ const Card = ({ dog }) => {
   });
 
   useEffect(() => {
-    const getDog = async (url) => {
-      try {
-        const response = await fetch(url);
-
-        if (!response.ok) throw new Error(response.status);
-
-        let [data] = await response.json();
-        const newCard = {
-          ...card,
-          name: data['breeds'][0]['name'],
-          url: data.url,
-        };
-        setCard(newCard);
-      } catch (err) {
-        console.error(err);
-      }
-    };
     if (dog !== null && dog !== '') {
-      getDog(`https://api.TheDogAPI.com/v1/images/search?breed_ids=${dog}`);
+      getDog(
+        `https://api.TheDogAPI.com/v1/images/search?breed_ids=${dog}`,
+        card,
+        setCard
+      );
     }
   }, [dog]);
 
   return (
-    <div className='card bounce'>
+    <div
+      className='card bounce'
+      onClick={() => {
+        getDog(
+          `https://api.TheDogAPI.com/v1/images/search?breed_ids=${dog}`,
+          card,
+          setCard
+        );
+      }}
+    >
       <img src={card.url} alt={card.name} />
       <p>{card.name}</p>
     </div>
